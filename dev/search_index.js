de83fcebb86b4f609dc8ac6212748511c0233d05
Documenter.jl <documenter@juliadocs.github.io>
2021-06-21 09:00:49 +0000

    build based on 4cd54d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StructHelpers","category":"page"},{"location":"#StructHelpers","page":"Home","title":"StructHelpers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StructHelpers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StructHelpers]","category":"page"}]
+}
